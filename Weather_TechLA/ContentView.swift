e8b1038cce358a395374ebd18dc0c35372de9434
Pavan <pgaridipuri@gmail.com>
2020-01-17 17:16:36 -0800

    Initial Commit

---
+++ Weather_TechLA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather_TechLA
+//
+//  Created by Pavan Garidipuri on 1/17/20.
+//  Copyright © 2020 Pavan Garidipuri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather_TechLATests/Weather_TechLATests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_TechLATests.swift
+//  Weather_TechLATests
+//
+//  Created by Pavan Garidipuri on 1/17/20.
+//  Copyright © 2020 Pavan Garidipuri. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_TechLA
+
+class Weather_TechLATests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
